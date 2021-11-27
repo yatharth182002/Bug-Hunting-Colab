b57a5c8adfa7bada390fb79222c0171987d46982
yatharth182002 <53784295+yatharth182002@users.noreply.github.com>
2021-11-27 11:41:43 +0530

    Update ttmg.py

--- ttmg.py
+++ ttmg.py
@@ -193,7 +193,7 @@ def _download(url, path):
         raise
 
 def argoTunnel():
-    _download("https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.tgz", "cloudflared.tgz")
+    _download("https://github.com/cloudflare/cloudflared/releases/download/2021.11.0/cloudflared-darwin-amd64.tgz", "cloudflared.tgz")
     shutil.unpack_archive("cloudflared.tgz")
     cfd_proc = subprocess.Popen(
         ["./cloudflared", "tunnel", "--url", "ssh://localhost:22", "--logfile", "cloudflared.log", "--metrics", "localhost:49589"],
